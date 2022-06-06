e5bb6065d6b20110713ebf2d39e83198b887d816
Polyakov Aleksey <apolyakov720@gmail.com>
2022-06-06 17:18:59 +0300

    Добавлено моковое апи для клиента

--- client/dev-server/server.js
+++ client/dev-server/server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const webpack = require('webpack');
@@ -10,8 +11,32 @@ const compilerInstance = webpackDevMiddleware(compiler);
 const devServerPort = process.env.DEV_SERVER_PORT || 3000;
 
 app.disable('x-powered-by');
-
 app.use(compilerInstance);
+
+app.use((req, res, next) => {
+  if (process.env.USE_MOCK_API) {
+    const { originalUrl } = req;
+    const originalPath = originalUrl.substring(1).split('/');
+    const directory = path.join(__dirname, `/${originalPath.shift()}/${originalPath.shift()}/`);
+    const fileName = originalPath.join('-').concat('.json');
+    const filePath = directory + fileName;
+
+    if (fs.existsSync(directory)) {
+      try {
+        return res.json(JSON.parse(fs.readFileSync(filePath, 'utf8')));
+      } catch (e) {
+        return res.json({
+          result: false,
+          messages: [
+            'Can\'t find folder or file',
+          ],
+        });
+      }
+    }
+  }
+
+  next();
+});
 
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
